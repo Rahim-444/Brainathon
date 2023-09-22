a64b97d94848a8e2a35a0e583796e9066421ef28
Rahim-444 <abderrahimbelkacemi33@gmail.com>
2023-09-22 19:55:55 +0100

    styled

--- brainathon/src/App.jsx
+++ brainathon/src/App.jsx
@@ -110,9 +110,9 @@ export const App = () => {
 
     function isNumValid(row, col, num, board) {
         for (let i = 0; i < 9; i++) {
-            if (board[row][i] === num) return false;
+            if ((board[row][i] === num) & (i !== col)) return false;
 
-            if (board[i][col] === num) return false;
+            if ((board[i][col] === num) & (i !== row)) return false;
 
             const currentMatrixRow = Math.floor(row / 3);
             const currentMatrixCol = Math.floor(col / 3);
@@ -120,7 +120,12 @@ export const App = () => {
             const currentRow = 3 * currentMatrixRow + Math.floor(i / 3);
             const currentCol = 3 * currentMatrixCol + (i % 3);
 
-            if (board[currentRow][currentCol] === num) return false;
+            if (
+                board[currentRow][currentCol] === num &&
+                currentCol !== col &&
+                currentRow !== row
+            )
+                return false;
         }
         return true;
     }
@@ -128,13 +133,28 @@ export const App = () => {
     const Box = ({ value, index }) => {
         const changed = (event) => {
             const updatedBoard = [...updatedboard];
-                        updatedBoard[event.target.name] = +event.target.value;
-            setSudokuBoard(updatedboard);
+            updatedBoard[event.target.name] = +event.target.value;
+            setSudokuBoard(updatedBoard);
         };
         return (
             <>
                 <input
                     id="box"
+                    style={(() => {
+                        const board = convert(SudokuBoard);
+                        console.log(
+                            isNumValid(Math.floor(index / 9), index % 9, value, board),
+                        );
+                        console.log(Math.floor(index / 9), index % 9, value, board);
+                        if (value === null) {
+                            return { background: "white" };
+                        }
+                        if (isNumValid(Math.floor(index / 9), index % 9, value, board)) {
+                            return { background: "#1b6083" };
+                        } else {
+                            return { background: "red" };
+                        }
+                    })()}
                     name={index}
                     type="text"
                     placeholder={value}
@@ -144,35 +164,31 @@ export const App = () => {
         );
     };
     const Board = ({ SudokuBoard }) => {
-         return (
-     <div className="board">
-            {SudokuBoard.map((value, index) => {
-                return <Box key={index} value={value} index={index} />;
-            })}
-        </div>
-     );
+        return (
+            <div className="board">
+                {SudokuBoard.map((value, index) => {
+                    return <Box key={index} value={value} index={index} />;
+                })}
+            </div>
+        );
     };
-    const showsol = () => {
+    const convert = (SudokuBoard) => {
         let board = Array.from({ length: 9 }, () => Array(9).fill(null));
         for (let i = 0; i < SudokuBoard.length; i++) {
             board[Math.floor(i / 9)][i % 9] = SudokuBoard[i];
         }
+        return board;
+    };
+    const showsol = () => {
+        const board = convert(SudokuBoard);
         solveSudoku(board);
-        const updatedBoard = [...updatedboard]; // Create a new array
+        const updatedBoard = [...SudokuBoard]; // Create a new array
         for (let i = 0; i < SudokuBoard.length; i++) {
             updatedBoard[i] = board[Math.floor(i / 9)][i % 9];
         }
         setSudokuBoard(updatedBoard);
-     };
-     // if (updatedboard === SudokuBoard) {
-    //     alert("You have already solved the sudoku");
-    // } else {
-    //     alert("You have solved the sudoku");
-     // }
-     //
-    //     const newBoard = { ...updatedboard };
-    //     setSudokuBoard(newBoard);
-    // };
+    };
+
     return (
         <div className="App">
             <h1 id="title">Sudoku</h1>
@@ -180,7 +196,7 @@ export const App = () => {
                 Fill all the Sudoku boxes to win numbers range from (1-9)
             </h3>
             <Board SudokuBoard={SudokuBoard} />
-            <button onClick={showsol}>Submit</button>
+            <button onClick={showsol}>Solve</button>
         </div>
     );
 };
